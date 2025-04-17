6841bdf98e1213aab4d8e4ff0748f1eaf1db6d0c
Cadu3011 <cadulinker132@gmail.com>
2025-04-16 23:14:15 -0300

    adição do Oauth

--- web/src/app/api/auth/[...nextauth]/options.ts
+++ web/src/app/api/auth/[...nextauth]/options.ts
@@ -2,7 +2,12 @@ import type { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
+import  jwt, { JwtPayload }  from "jsonwebtoken";
 
+interface MyPayload extends JwtPayload {
+  email: string;
+  name?: string;
+}
 export const options: NextAuthOptions = {
   providers: [
     GithubProvider({
@@ -16,7 +21,7 @@ export const options: NextAuthOptions = {
     CredentialsProvider({
       name: "Credentials",
       credentials: {
-        username: {
+        email: {
           label: "Username",
           type: "text",
           placeholder: "Enter your username",
@@ -26,24 +31,60 @@ export const options: NextAuthOptions = {
           type: "password",
           placeholder: "Enter your password",
         },
-        confirmPassword: {
-            label: "confirm password",
-            type: "confirmpassword",
-            placeholder: "confirm your password",
-          },
       },
+      
       async authorize(credentials) {
-        const user = { id: "1001", name: "Saleem", password: "12345" };
-        if (
-          credentials?.username === user.name &&
-          credentials?.password === user.password &&
-          credentials?.confirmPassword ===user.password
-        ) {
-          return user;
-        } else {
+        if (!credentials?.email || !credentials?.password) {
+          console.error("Email ou senha estão vazios!");
           return null;
         }
-      },
+      
+        try {
+          const res = await fetch("http://localhost:4000/auth/signin", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              email: credentials.email,
+              password: credentials.password,
+            }),
+          });
+      
+          if (!res.ok) {
+            throw new Error("Falha na autenticação");
+          }
+          
+          const user = await res.json();
+          if (!user.access_token) {
+            console.error("Token não fornecido");
+            return null;
+          }
+          const decodeUser =  jwt.decode(user.access_token) as MyPayload
+          
+          if (!decodeUser) {
+            console.error("Token inválido ou incompleto");
+            return null;
+          }
+          
+          
+          return {
+            id: String(decodeUser.sub),
+            email: decodeUser.email,
+            name: decodeUser.name ?? "",
+            image: "/"
+          };
+        } catch (error) {
+          console.error("Erro ao autorizar usuário:", error);
+          return null;
+        }
+      }
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      console.log(user.name)
+      return true
+    }
+  }
 };
